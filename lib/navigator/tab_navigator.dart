aa05c3118f45e9a227521c22414180754a68f9aa
蒲盛坤 <3528816081@qq.com>
2020-08-26 10:59:09 +0800

    添加tab栏

---
+++ lib/navigator/tab_navigator.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_pocket_book/pages/home_page.dart';
+import 'package:flutter_pocket_book/pages/mine_page.dart';
+import 'package:curved_navigation_bar/curved_navigation_bar.dart';
+
+class TabNavigator extends StatefulWidget {
+  @override
+  _TabNavigatorState createState() => _TabNavigatorState();
+}
+
+class _TabNavigatorState extends State<TabNavigator> {
+  final PageController _controller = PageController(initialPage: 0);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: PageView(
+        // 滑动超出边界时的动画效果
+        // 类ios效果： BouncingScrollPhysics
+        // 类安卓效果： AlwaysScrollableScrollPhysics
+        // 无效果： NeverScrollableScrollPhysics
+        physics:  AlwaysScrollableScrollPhysics(),
+        controller: _controller,
+        children: <Widget>[
+          HomePage(),
+          MinePage(),
+        ],
+      ),
+      bottomNavigationBar: CurvedNavigationBar(
+        backgroundColor: Color(0xFFfafafa),
+        items: <Widget>[
+          Icon(Icons.list, size: 30),
+          Icon(Icons.add, size: 30),
+          Icon(Icons.account_circle, size: 30),
+        ],
+        onTap: (index) {
+          print(index);
+          //Handle button tap
+        },
+      ),
+    );
+  }
+}
---
+++ lib/pages/mine_page.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class MinePage extends StatefulWidget {
+  @override
+  _MinePageState createState() => _MinePageState();
+}
+
+class _MinePageState extends State<MinePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('我的'),
+      ),
+    );
+  }
+}
