6530b8abd465d9f86ef459640fedb32c7e3d5f2c
蒲盛坤 <3528816081@qq.com>
2020-08-28 15:05:44 +0800

    我的页面部分ui

---
+++ lib/pages/mine/mine_ad_widget.dart
@@ -0,0 +1,77 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_pocket_book/utils/img_util.dart';
+
+List adInfoMap = [
+  {
+    "imgUrl": "ic_me_yd_tab_ad.png",
+    "title": "广告投放",
+    "desc": "广告",
+  },
+  {
+    "imgUrl": "ic_me_yd_tab_ad.png",
+    "title": "广告投放",
+    "desc": "广告",
+  },
+  {
+    "imgUrl": "ic_me_yd_tab_ad.png",
+    "title": "广告投放",
+    "desc": "广告",
+  },
+];
+
+class MineAd extends StatefulWidget {
+  @override
+  _MineAdState createState() => _MineAdState();
+}
+
+class _MineAdState extends State<MineAd> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.only(top: 28.0, bottom: 20.0),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: adBuilder(),
+      ),
+    );
+  }
+}
+
+// 广告位 创建
+List<Widget> adBuilder() {
+  List<Widget> result = new List();
+  adInfoMap.forEach((item) {
+    result.add(Expanded(
+      flex: 1,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          Image.asset(
+            Img.getAssetsImg(item["imgUrl"]),
+            fit: BoxFit.cover,
+            height: 35.0,
+            width: 35.0,
+          ),
+          SizedBox(height: 8.0),
+          Text(
+            item["title"],
+            style: TextStyle(
+              fontSize: 13.0,
+              color: Colors.black,
+            ),
+          ),
+          SizedBox(height: 3.0),
+          Text(
+            item["desc"],
+            style: TextStyle(
+              fontSize: 10.0,
+              color: Colors.grey,
+            ),
+          ),
+        ],
+      ),
+    ));
+  });
+  return result.toList();
+}
--- lib/pages/mine/mine_page.dart
+++ lib/pages/mine/mine_page.dart
@@ -3,6 +3,8 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_pocket_book/utils/img_util.dart';
 
+import 'mine_ad_widget.dart';
+
 class MinePage extends StatefulWidget {
   @override
   _MinePageState createState() => _MinePageState();
@@ -17,6 +19,7 @@ class _MinePageState extends State<MinePage> {
     return Scaffold(
       body: Column(
         children: <Widget>[
+          // 顶部按钮
           Container(
             margin: EdgeInsets.only(top: MediaQuery.of(context).padding.top),
             padding: EdgeInsets.only(right: 10.0, top: 10.0, bottom: 10.0),
@@ -51,7 +54,55 @@ class _MinePageState extends State<MinePage> {
                 ),
               ],
             ),
-          )
+          ),
+          // 用户信息
+          GestureDetector(
+            onTap: () {
+              Navigator.pushNamed(context, '/login');
+            },
+            child: Row(
+              children: <Widget>[
+                Padding(
+                  padding: EdgeInsets.only(left: 20.0),
+                  child: Image.asset(
+                    Img.getAssetsImg("ic_vip_default_avatar.png"),
+                    height: 65.0,
+                    width: 65.0,
+                  ),
+                ),
+                Container(
+                  margin: EdgeInsets.only(left: 15.0),
+                  height: 58.0,
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: <Widget>[
+                      Text(
+                        "立即登陆",
+                        style: TextStyle(
+                          fontSize: 18.0,
+                          fontWeight: FontWeight.w700,
+                        ),
+                      ),
+                      Text(
+                        "登陆后，将会拥有更好的使用体验",
+                        style: TextStyle(
+                          fontSize: 13.0,
+                          color: Colors.grey,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+//
+              ],
+            ),
+          ),
+          MineAd(),
+          Container(
+            height: 10.0,
+            color: Color.fromARGB(255, 245, 245, 245),
+          ),
         ],
       ),
     );
